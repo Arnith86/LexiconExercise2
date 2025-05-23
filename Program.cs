9d37c2ea2859489988f49b104f7dd37255e744ee
a21jeaha <hisui86@gmail.com>
2025-05-23 15:12:48 +0200

    implemented the main menue and added the first option 0: exit program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome, this is the main menu. \n" +
+                "To test the features bellow input its corresponding number!\n");
+
+            bool exitProgram = false;
+
+            string input = string.Empty;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+				Console.WriteLine("0: Exit the program.");
+                Console.ResetColor();
+
+                input = Console.ReadLine();
+
+				switch (input)
+				{
+                    case "0": exitProgram = true;
+                        break;
+
+					default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You supplied an invalid input, try again!");
+                        Console.ResetColor();
+                        break;
+				}
+            }
+            while (!exitProgram);
         }
     }
 }
