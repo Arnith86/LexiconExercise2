21a1606b20993a55353cc7c3bbe69d338448c817
a21jeaha <hisui86@gmail.com>
2025-05-26 15:53:29 +0200

    Restructure: moved files to new folder layout

---
+++ MenuHelpers/MenuHelpers.cs
@@ -0,0 +1,29 @@
+﻿namespace LexiconExercise2.MenuHelpers
+{
+	public class MainManuHelpers
+	{
+		public const string EXIT = "0";
+		public const string CINEMA_PRICING = "1";
+		public const string INPUT_ECHO = "2";
+		public const string THIRD_WORD_STRING_SPLITTER = "3";
+	}
+
+	public class CinemaPricingMenuHelpers
+	{
+		public const string RETURN_TO_MAIN_MENU = "0";
+		public const string SINGLE_VISITOR = "1";
+		public const string GROUP_OF_VISITORS = "2";
+	}
+
+	public class InputEchoMenuHelpers
+	{
+		public const string RETURN_TO_MAIN_MENU = "0";
+		public const string SHOUT_INTO_CHAMBER = "1";
+	}
+	
+	public class ThirdWordStringSplitterMenuHelpers
+	{
+		public const string RETURN_TO_MAIN_MENU = "0";
+		public const string FIND_THIRD_WORD = "1";
+	}
+}
