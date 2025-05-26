52ac566ff8a56210221d1bf19bc803914757cb0d
a21jeaha <hisui86@gmail.com>
2025-05-26 16:05:56 +0200

    All logic moved out of Program to the newly created class MainMenu

---
+++ LexiconExercise2/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using LexiconExercise2.MenuHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise2
+{
+	internal class MainMenu
+	{
+
+		private ICinemaPricingHelper _cinemaPricingHelper;
+		private IInputEcho _inputEcho;
+		private IThirdWordStringSplitter _thirdWordStringSplitter;
+
+		public MainMenu(ICinemaPricingHelper cinemaPricingHelper, IInputEcho inputEcho, IThirdWordStringSplitter thirdWordStringSplitter)
+		{
+			_cinemaPricingHelper = cinemaPricingHelper;
+			_inputEcho = inputEcho;
+			_thirdWordStringSplitter = thirdWordStringSplitter;
+		}
+
+		public void DisplayMainMenu()
+		{
+			DisplayHeaders.DisplayHeaderText(
+			   "Welcome, this is the main menu. \n" +
+			   "To test the features bellow input its corresponding number!\n"
+		   );
+
+			bool exitProgram = false;
+
+			string input = string.Empty;
+
+			do
+			{
+				//TODO: create menu display class.
+				Console.ForegroundColor = ConsoleColor.DarkGray;
+				Console.WriteLine("0: Exit the program.");
+				Console.WriteLine("1: Cinema Pricing.");
+				Console.WriteLine("2: Input Echo. Writes back input multiple times.");
+				Console.WriteLine("3: String Splitter - Which is the third word?");
+				Console.ResetColor();
+
+				input = Console.ReadLine();
+
+				switch (input)
+				{
+					case MainManuHelpers.EXIT:
+						exitProgram = true;
+						break;
+					case MainManuHelpers.CINEMA_PRICING:
+						_cinemaPricingHelper.CinemaPricingMenu();
+						break;
+					case MainManuHelpers.INPUT_ECHO:
+						_inputEcho.InputEchoMenu();
+						break;
+					case MainManuHelpers.THIRD_WORD_STRING_SPLITTER:
+						_thirdWordStringSplitter.ThirdWordStringSplitterMenu();
+						break;
+					default:
+						ErrorMessages.InvalidIntInput();
+						break;
+				}
+			}
+			while (!exitProgram);
+		}
+	}
+}
--- LexiconExercise2/Program.cs
+++ LexiconExercise2/Program.cs
@@ -7,53 +7,14 @@ namespace LexiconExercise2
         static void Main(string[] args)
         {
             //TODO: wrapper class for con in and con out, abstraction
-            //TODO: move code from program to a "main" class
-
+            
 			ICinemaPricingHelper cinemaPricingHelper = new CinemaPricingHelper();
             IInputEcho inputEcho = new InputEcho();
             IThirdWordStringSplitter thirdWordStringSplitter = new ThirdWordStringSplitter();
 
-            DisplayHeaders.DisplayHeaderText(
-                "Welcome, this is the main menu. \n" +
-                "To test the features bellow input its corresponding number!\n"
-            );
+            MainMenu mainMenu = new MainMenu(cinemaPricingHelper, inputEcho, thirdWordStringSplitter);
 
-            bool exitProgram = false;
-
-            string input = string.Empty;
-
-            do
-            {
-                //TODO: create menu display class.
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-				Console.WriteLine("0: Exit the program.");
-                Console.WriteLine("1: Cinema Pricing.");
-                Console.WriteLine("2: Input Echo. Writes back input multiple times.");
-                Console.WriteLine("3: String Splitter - Which is the third word?");
-                Console.ResetColor();
-
-                input = Console.ReadLine();
-
-				switch (input)
-				{
-                    case MainManuHelpers.EXIT: 
-                        exitProgram = true;
-                        break;
-                    case MainManuHelpers.CINEMA_PRICING:
-                        cinemaPricingHelper.CinemaPricingMenu();
-                        break;
-                    case MainManuHelpers.INPUT_ECHO:
-                        inputEcho.InputEchoMenu();
-                        break;
-                    case MainManuHelpers.THIRD_WORD_STRING_SPLITTER:
-                        thirdWordStringSplitter.ThirdWordStringSplitterMenu();
-                        break;
-					default:
-						ErrorMessages.InvalidIntInput();
-                        break;
-				}
-            }
-            while (!exitProgram);
+            mainMenu.DisplayMainMenu();
         }
     }
 }
