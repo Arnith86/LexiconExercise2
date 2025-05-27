2237b3b76783c386333d87ea29e64e6bff68bb40
a21jeaha <hisui86@gmail.com>
2025-05-27 15:04:23 +0200

    created I & ValidateTextInput, used for validating inputs from the console in various ways

---
+++ LexiconExercise2/Util/IValidateTextInput.cs
@@ -0,0 +1,32 @@
+﻿using LexiconExercise2.Util.DisplayTextClasses;
+
+namespace LexiconExercise2.Util
+{
+	internal interface IValidateTextInput
+	{
+		/// <summary>
+		/// Validates input expected to have a minimum of one character. 
+		/// </summary>
+		/// <param name="requestMessage">The message displayed to user, requesting the specified input.</param>
+		/// <returns>A validated string.</returns>
+		string ValidateStringInput(string requestMessage);
+
+		/// <summary>
+		/// Validates input expected to be in uint format. 
+		/// </summary>
+		/// <param name="requestMessage">The message displayed to user, requesting the specified input.</param>
+		/// <returns>uint converted from the string input.</returns>
+		uint ValidateTextIntInput(string requestMessage);
+
+
+		/// <summary>
+		/// Validates input expected to be a menu selection, i.e. a number within a specified range.
+		/// </summary>
+		/// <param name="requestMessage">The message displayed to user, requesting the specified input.</param>
+		/// <param name="rangeMin">Start value of the menu range.</param>
+		/// <param name="rangeMax">End value of the menu range.</param>
+		/// <returns>uint converted from the string input.</returns>
+		string ValidateMenuInput(string requestMessage, int rangeMin, int rangeMax);
+		
+	}
+}
---
+++ LexiconExercise2/Util/ValidateTextInput.cs
@@ -0,0 +1,100 @@
+﻿using LexiconExercise2.Util.DisplayTextClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiconExercise2.Util
+{
+	internal class ValidateTextInput : IValidateTextInput
+	{
+		private readonly IReadAndWriteToConsole _readAndWriteToConsole;
+		private readonly DisplayTextWrapper _displayTextWrapper;
+
+		public ValidateTextInput(
+			IReadAndWriteToConsole readAndWriteToConsole,
+			DisplayTextWrapper displayTextWrapper
+			)
+		{
+			_readAndWriteToConsole = readAndWriteToConsole;
+			_displayTextWrapper = displayTextWrapper;
+		}
+
+		/// <inheritdoc/>
+		public string ValidateStringInput(string requestMessage)
+		{
+			_readAndWriteToConsole.Print(requestMessage);
+
+			string input = string.Empty;
+
+			while (true)
+			{
+				input = _readAndWriteToConsole.ReadInput();
+
+				if (string.IsNullOrWhiteSpace(input))
+					_displayTextWrapper.DisplayErrorMessages.InvalidInputEmpty();
+				else 
+					break; // input is valid, exit loop.
+			}
+
+			return input;
+		}
+
+		/// <inheritdoc/>
+		public uint ValidateTextIntInput(string requestMessage)
+		{
+			_readAndWriteToConsole.Print(requestMessage);
+
+			string input = string.Empty;
+			uint result = 0;
+
+			while (true)
+			{
+				input = _readAndWriteToConsole.ReadInput();
+
+				if (string.IsNullOrWhiteSpace(input))
+					_displayTextWrapper.DisplayErrorMessages.InvalidInputEmpty();
+				else if (!uint.TryParse(input, out uint uInt))
+					_displayTextWrapper.DisplayErrorMessages.InvalidInputNegativeValue();
+				else
+				{
+					result = uInt;
+					break; // input is valid, exit loop.
+				}
+			}
+
+			return result;
+		}
+
+		/// <inheritdoc/>
+		public string ValidateMenuInput(string requestMessage, int rangeMin, int rangeMax)
+		{
+			_displayTextWrapper.DisplayMenu.DisplayMenuText(requestMessage);
+
+			string input = string.Empty;
+			
+			while (true)
+			{
+				input = _readAndWriteToConsole.ReadInput();
+
+				if (string.IsNullOrWhiteSpace(input))
+				{
+					 _displayTextWrapper.DisplayErrorMessages.InvalidInputEmpty();
+				}
+				// Tries to convert input to int, and check if it is within menu range.
+				else if (
+					!int.TryParse(input, out int intInput) || 
+					intInput < rangeMin ||	
+					intInput > rangeMax)
+				{
+					_displayTextWrapper.DisplayErrorMessages.InvalidMenuInput();
+				}
+				else
+					break; // input is valid, exit loop.
+			}
+
+			return input;
+		}
+	}
+}
